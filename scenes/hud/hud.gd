5cfa999066a708c42e961759840b540187af625d
kinghos <62035531+kinghos@users.noreply.github.com>
2026-07-23 22:02:10 +0100

    fix progress bar

--- scenes/hud/hud.gd
+++ scenes/hud/hud.gd
@@ -14,5 +14,5 @@ func _process(delta: float) -> void:
 	timer.text = "TIME: %02d:%02d" % [mins, secs]
 	
 	var perc = Globals.current_kills_to_target / Globals.current_kill_target
-	progress_bar.value = perc
+	progress_bar.value = lerpf(progress_bar.value, perc, delta * 15)
 	
