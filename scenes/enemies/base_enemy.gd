e0327a786f462bb44abc71a5c50a8f349ec2dce9
kinghos <62035531+kinghos@users.noreply.github.com>
2026-07-23 11:38:12 +0100

    enemy follow

--- scenes/enemies/base_enemy.gd
+++ scenes/enemies/base_enemy.gd
@@ -4,4 +4,11 @@ class_name Enemy
 var enemy_type;
 var hp;
 var damage;
-var speed;
+@export var speed = 0;
+
+func attack_player():
+	velocity = global_position.direction_to(Globals.player.global_position) * speed
+	move_and_slide()
+
+func _process(delta: float) -> void:
+	attack_player()
