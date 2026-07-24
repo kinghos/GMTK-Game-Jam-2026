ccf7fad7dc536728056ae51eb88bba01ea8abe54
kinghos <62035531+kinghos@users.noreply.github.com>
2026-07-24 21:56:28 +0100

    death anim

--- scenes/player/death_animation.gd
+++ scenes/player/death_animation.gd
@@ -1,9 +1,10 @@
-extends GPUParticles2D
+extends CPUParticles2D
 
-@onready var audio_stream_player_2d = $AudioStreamPlayer2D
+@onready var audio_stream_player_2d = $AudioStreamPlayer
 
 func _ready():
 	emitting = true
 	audio_stream_player_2d.play()
 	await get_tree().create_timer(lifetime).timeout
+	get_tree().change_scene_to_file("res://scenes/hud/game_over.tscn")
 	get_parent().queue_free()
