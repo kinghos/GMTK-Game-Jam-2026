e61dfdbfd54caba6e5c2cf92d2a7c2ec95652be9
kinghos <62035531+kinghos@users.noreply.github.com>
2026-07-24 12:24:53 +0100

    theming + powerup changes

--- scenes/hud/overlay.gd
+++ scenes/hud/overlay.gd
@@ -1,8 +1,19 @@
 extends CanvasLayer
 
 @onready var animation_player: AnimationPlayer = $PowerupScreen/AnimationPlayer
+@onready var options: HBoxContainer = $PowerupScreen/PowerupsMenu/Options
 
 func _on_hud_show_powerups() -> void:
+	var selection = Globals.POWERUPS.keys()
+	selection.shuffle()
+	selection = selection.slice(0, 3)
+	for i in range(len(selection)):
+		selection[i] = Globals.POWERUPS[selection[i]]
+	var options = options.get_children()
+	for i in range(3):
+		options[i].set_meta("powerup_type", selection[i])
+		options[i].icon = Globals.powerup_icons[selection[i]]
+		options[i].text = Globals.powerup_names[selection[i]]
 	animation_player.play("powerups")
 
 
