5e5e4da0717404b5d714a4580c8962b870de5967
MTOnline69 <97001154+MTOnline69@users.noreply.github.com>
2026-07-25 21:50:44 +0100

    cutscene

--- scenes/hud/title_screen.gd
+++ scenes/hud/title_screen.gd
@@ -4,8 +4,11 @@ func _ready() -> void:
 	Music.play_music(Music.MAIN_THEME)
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level/level.tscn")
+	get_tree().change_scene_to_file("res://scenes/ui/menus/opening_cutscene.tscn")
 
 
 func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/hud/credits.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
